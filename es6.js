8f10f2e34712b4f7e28e6ec89066210e744cd886
chhayapk <120550648+chhayapk@users.noreply.github.com>
2023-02-08 19:00:16 +0530

    Add files via upload

--- es6.js
+++ es6.js
@@ -156,3 +156,143 @@ let sum1 = (a, b) => {
 let result1 = sum1(5,7);
 console.log(result1); // 12
 
+//inside an arrow function
+function Person3() {
+    this.name = 'Jack',
+    this.age = 25,
+    this.sayName = function () {
+
+        console.log(this.age);
+        let innerFunc = () => {
+            console.log(this.age);
+        }
+
+        innerFunc();
+    }
+}
+
+//Arguments Binding
+
+//Regular functions have arguments binding. 
+const x = new Person3();
+x.sayName();
+
+let m = function () {
+    console.log(arguments);
+}
+m(4,6,7); // Arguments [4, 6, 7]
+
+
+//Arrow functions do not have arguments binding. It gives errors. So we use spread syntax
+let r = (...n) => {
+    console.log(n);
+}
+
+r(4,6,7); // [4, 6, 7]
+
+
+// ES6
+//asyncFunction()
+//.then(() => asyncFunction1())
+//.then(() => asyncFunction2())
+//.then(() => finish);
+
+//The concept of default parameters is a new feature introduced in the ES6 version of JavaScript. This allows us to give default values to function parameters. Let's take an example,
+
+//default parameters
+function sum(x = 3, y = 5) {
+
+    // return sum
+    return x + y;
+}
+
+console.log(sum(5, 15));  // 20 
+console.log(sum(7));        // 12
+console.log(sum());          // 8
+
+
+//Passing Parameter as Default Values
+function sum_1(a = 1, b = a,  c = a + b) {
+    console.log( a + b + c );
+}
+
+sum_1(); // 4
+
+//If you reference the parameter that has not been initialized yet, you will get an error
+//function sum( x = y, y = 1 ) {
+//    console.log( x + y);
+//}
+
+//sum(); 
+
+//Passing Function Value as Default Value
+// using a function in default value expression
+
+const sum_2 = () => 15;
+
+const calculate = function( x, y = x * sum_2() ) {
+    return x + y;
+}
+
+const result = calculate(10);
+console.log(result);            // 160
+
+
+//Passing undefined Value
+//In JavaScript, when you pass undefined to a default parameter function, the function takes the default value. For example,
+
+function test(x = 1) {
+    console.log(x);
+  }
+  
+  // passing undefined
+  // takes default value 1
+  test(undefined); // 1
+
+  //Template literals
+const name_1 = 'Jack';
+console.log(`Hello ${name_1}!`); // Hello Jack!
+
+//Template Literals for Strings
+const str1 = `This is a string`;
+const str2 = `This is a string with a 'quote' in it`;
+const str3 = `This is a string with a "double quote" in it`;
+
+//Multiline Strings Using Template Literals
+const message1 = `This is a long message
+that spans across multiple lines
+in the code.`
+
+console.log(message1)
+
+//Expression Interpolation
+const name_3 = 'Jack';
+console.log(`Hello ${name_3}`); 
+
+const result_3 = 4 + 5;
+const result_4 = 10 + 5;
+
+// template literals used with expressions
+console.log(`The sum of 4 + 5 is ${result_3}`);
+
+console.log(`${result_3 < 10 ? 'Too low': 'Very high'}`)
+console.log(`${result_4 < 10 ? 'Too low': 'Very high'}`)
+
+//tagged templates
+const name_4 = 'Jack';
+const greet_4 = true;
+
+function tagExample(strings, nameValue) {
+    let str0 = strings[0]; // Hello
+    let str1 = strings[1]; // , How are you?
+
+    if(greet) {
+        return `${str0}${nameValue}${str1}`;
+    }
+}
+
+// creating tagged literal
+// passing argument name
+const result_5 = tagExample`Hello ${name_4}, How are you?`;
+
+console.log(result_5);
